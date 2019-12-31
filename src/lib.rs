fc78dba9d7e84c0f939d6d747e757609042fa37a
leekt216 <leekt216@gmail.com>
2019-12-31 18:02:04 +0900

    permutation table. need to check if -1 is valid

--- src/lib.rs
+++ src/lib.rs
@@ -9,18 +9,35 @@
 /// fn inverseIP(R16, L16) -> 64bit;
 
 
-fn initialPermutation(i: u64) -> u64 {
-    
+fn permutation_table(x:u8) -> u8 {
+    let table = [
+        58,50,42,34,26,18,10,02,
+        60,52,44,36,28,20,12,04,
+        62,54,46,38,30,22,14,06,
+        64,56,48,40,32,24,16,08,
+        57,49,41,33,25,17,09,01,
+        59,51,46,35,27,19,11,03,
+        61,33,45,37,29,21,13,05,
+        63,55,47,39,31,23,15,07,
+    ];
+    table[x as usize] - 1
 }
 
-fn finalPermutation(i: u64) -> u64) {
-
+fn initial_permutation(input: &[u8;8]) -> [u8;8] {
+    let mut r : [u8; 8] = [0 as u8;8];
+    for i in 0..8 {
+        r[i] = permutation_table(input[i] - 1);
+    }
+    r.clone()
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::*;
     #[test]
     fn it_works() {
+        let x = initial_permutation(&[3,3,3,3,3,3,3,3]);
+        dbg!(x);
         assert_eq!(2 + 2, 4);
     }
 }
