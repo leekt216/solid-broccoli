67ec16acaf2c78dc1c5a45ae17ce90b2466ee242
leekt216 <leekt216@gmail.com>
2020-01-12 20:26:19 +0900

    key split

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(const_generics)]
 /// DES on (K,I) -> R
 /// K : key, 56 bit
 /// I : input, 64 bit 
@@ -17,14 +18,18 @@ const EXPANSION_TABLE : [u8; 48] = [
 ];
 
 trait BitUtil {
-    fn get_bit(&self, index: u8) -> bool;
+    fn get_bit(&self, index: usize) -> bool;
     //TODO : change to Result?
-    fn set_bit(&mut self, index: u8) -> bool;
+    fn set_bit(&mut self, index: usize) -> bool;
+    fn rotate(&self, rhs: usize) -> Self;
+    fn rotate_as_bits(&self, rhs: usize, bits: usize) -> Self;
+    fn right_shift(&self, rhs: usize) -> Self;
+    fn left_shift(&self, rhs: usize) -> Self;
 }
 
-impl BitUtil for [u8] {
-    fn get_bit(&self, index: u8) -> bool {
-        if index > (self.len() *8 - 1) as u8 { false }
+impl<const N: usize> BitUtil for [u8; N] {
+    fn get_bit(&self, index: usize) -> bool {
+        if index >= N*8 { false }
         else {
             //get self index from index
             let s = index / 8;
@@ -34,8 +39,8 @@ impl BitUtil for [u8] {
     }
 
     //set bit to 1
-    fn set_bit(&mut self, index: u8) -> bool {
-        if index > (self.len() *8 - 1) as u8 { false }
+    fn set_bit(&mut self, index: usize) -> bool {
+        if index >= N*8 { false }
         else {
             //get self index from index
             let s = index / 8;
@@ -44,51 +49,52 @@ impl BitUtil for [u8] {
             true
         }
     }
-}
 
-#[derive(Clone, Copy)]
-struct Bit6 {
-    arr : [bool;6]
-}
-
-impl BitUtil for Bit6 {
-    fn get_bit(&self, index: u8) -> bool {
-        if index > 5 {
-            false
+    //rotate left
+    fn rotate(&self, rhs: usize) -> [u8; N] {
+        let mut x = self.clone();
+        for i in 0..N {
+            x[i] = self[i] << rhs;
+            let k = if i+1 == self.len()  {0} else {i+1};
+            x[i] += self[k] >> (8-rhs);
         }
-        else {
-            self.arr[index as usize]
-        }
+        x
     }
 
-    fn set_bit(&mut self, index: u8) -> bool {
-        if index > 5 {
-            false
+    //right shift 
+    fn right_shift(&self, rhs: usize) -> [u8; N] {
+        let mut x = self.clone();
+        x[0] = self[0] >> rhs;
+        for i in 1..N {
+            x[i] = self[i] >> rhs;
+            x[i] += self[i-1] << (8-rhs);
         }
-        else {
-            self.arr[index as usize] = true;
-            true
-        }
+        x
     }
-}
-
-impl Bit6 {
-    fn default() -> Bit6 {
-        Bit6 {
-            arr: [false;6]
+    
+    //left shift 
+    fn left_shift(&self, rhs: usize) -> [u8; N] {
+        let mut x = self.clone();
+        for i in 0..N-1 {
+            x[i] = self[i] << rhs;
+            x[i] += self[i+1] >> (8-rhs);
         }
+        x[N-1] = self[N-1] << rhs;
+        x
     }
 
-    fn from_48bit(from: [u8;6]) -> [Bit6;8] {
-        let mut r : [Bit6; 8] = [Bit6::default();8];
-        for i in 0..48 {
-            let r_j = i/6;
-            let r_k = i%6;
-            if from.get_bit(i) {
-                r[r_j as usize].set_bit(r_k);
-            }
+    fn rotate_as_bits(&self, rhs:usize, bits: usize) -> [u8; N] {
+        let mut x = self.rotate(rhs);
+        for i in 0..N {
+            x[i] = self[i] << rhs;
+            let (k, o) = if i+1 == self.len() {(0, N*8 - bits)} else {(i+1, 0)};
+            x[i] += self[k] >> (8 - rhs - o);
         }
-        r
+
+        for i in 0..(N*8 - bits) {
+            x[0] &= 255 as u8 >> i + 1;
+        }
+        x
     }
 }
 
@@ -97,7 +103,7 @@ trait Permutation {
     fn run(input: [u8;8]) -> [u8;8] {
         let mut r : [u8;8] = Default::default();
         for i in 0..64 {
-            if input.get_bit(Self::TABLE[i as usize]) {
+            if input.get_bit(Self::TABLE[i as usize] as usize) {
                 r.set_bit(i);
             }
         }
@@ -134,7 +140,7 @@ impl Permutation for FinalPermutation {
     ];
 }
 
-fn split(input: [u8;8]) -> ([u8;4], [u8;4]) {
+fn split_64bit(input: [u8;8]) -> ([u8;4], [u8;4]) {
     let mut l : [u8;4] = Default::default();
     l.copy_from_slice(&input[0..4]);
     let mut r : [u8;4] = Default::default();
@@ -142,10 +148,46 @@ fn split(input: [u8;8]) -> ([u8;4], [u8;4]) {
     (l,r)
 }
 
+fn split_56bit(input: [u8;7]) -> ([u8;4], [u8;4]) {
+    let mut l : [u8;4] = Default::default();
+    l.copy_from_slice(&input[0..4]);
+    l = l.right_shift(4);
+    let mut r : [u8;4] = Default::default();
+    r.copy_from_slice(&input[3..7]);
+    r = r.left_shift(4).right_shift(4);
+    (l,r)
+}
+
+const PARITY_DROP_TABLE: [u8;56] = [
+    56, 48, 40, 32, 24, 16, 8, 0,
+    57, 49, 41, 33, 25, 17, 9, 1,
+    58, 50, 42, 34, 26, 18, 10, 2,
+    59, 51, 43, 35, 62, 54, 46, 38,
+    30, 22, 14, 6, 61, 53, 45, 37,
+    29, 21, 13, 5, 60, 52, 44, 36,
+    28, 20, 12, 4, 27, 19, 11, 3
+];
+
+fn parity_drop(input: [u8;8]) -> [u8;7] {
+    let mut r : [u8;7] = Default::default();
+    for i in 0..56 {
+        if input.get_bit(PARITY_DROP_TABLE[i as usize] as usize)  {
+            r.set_bit(i);
+        }
+    }
+    r.clone()
+}
+
+/*
+fn round_keys(input: [u8;8]) ->[[u8;6];16]{
+    let mut r : [[u8;6]; 16] = Default::default();
+}
+
+*/
 fn expand(input: [u8;4]) -> [u8;6] {
     let mut r : [u8;6] = Default::default();
     for i in 0..48{
-        if input.get_bit(EXPANSION_TABLE[i as usize]) {
+        if input.get_bit(EXPANSION_TABLE[i as usize] as usize) {
             r.set_bit(i);
         }
     }
@@ -165,7 +207,7 @@ mod DESTests {
     #[test]
     fn should_split_correctly() {
         let input = 14_699_974_583_363_760_298 as u64;
-        let (l,r) = split(input.to_be_bytes());
+        let (l,r) = split_64bit(input.to_be_bytes());
         assert_eq!((l,r), ((3_422_604_543 as u32).to_be_bytes(), (4_037_734_570 as u32).to_be_bytes()));
     }
 
@@ -174,5 +216,40 @@ mod DESTests {
         let input = 4_037_734_570 as u32;
         let expanded = expand(input.to_be_bytes());
         assert_eq!(expanded, (134_232_046_966_101 as u64).to_be_bytes()[2..8]);
+    }
+
+    #[test]
+    fn should_drop_parity_correctly() {
+        let input = 1383827165325090801 as u64;
+        let dropped = parity_drop(input.to_be_bytes());
+        assert_eq!(dropped, (67779029043144591 as u64).to_be_bytes()[1..8]);
+    }
+
+    #[test]
+    fn should_rotate_correctly() {
+        let input = [127 as u8, 0 as u8];
+        let rotated = input.rotate(1);
+        assert_eq!(rotated, [254 as u8, 0 as u8]);
+    }
+
+    #[test]
+    fn should_rotate_partial_correctly() {
+        let input = [127 as u8, 0 as u8];
+        let rotated = input.rotate_as_bits(1,15);
+        assert_eq!(rotated, [126 as u8, 1 as u8]);
+    }
+
+    #[test]
+    fn should_shift_correctly(){
+        let input = [127 as u8, 0 as u8];
+        let rotated = input.right_shift(1);
+        assert_eq!(rotated, [63 as u8, 128 as u8]);
+    }
+
+    #[test]
+    fn should_split_56bit_correctly() {
+        let input = [255 as u8;7];
+        let (l,r) = split_56bit(input);
+        assert_eq!((l,r),([15, 255,255,255],[15,255,255,255]));
     }
 }
